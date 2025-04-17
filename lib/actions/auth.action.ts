fa6bf870fca7c98d552fb43d699dd1af8d3b7ed5
saroj580 <chaudharysaroj819@gmail.com>
2025-04-18 01:09:20 +0530

    prevent homepage before authentication

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -74,3 +74,29 @@ export async function setSessionCookie(idToken:string) {
         
     })
 }
+
+export async function getCurrentUser() : Promise<User | null > {
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get('session')?.value;
+    if (!sessionCookie) return null;
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+        const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+
+        if (!userRecord.exists) return null; 
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentUser();
+    return !!user; 
+
+}

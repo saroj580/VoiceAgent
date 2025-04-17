ad11ef13988efebf102ace8db779aaa193503ce1
saroj580 <chaudharysaroj819@gmail.com>
2025-04-18 01:15:57 +0530

    modified

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCL1LkB1vAcs2w5su7A6-iU0kPi45qGKu0",
+  apiKey: process.env.APIKEY,
   authDomain: "prepwise-2a975.firebaseapp.com",
   projectId: "prepwise-2a975",
   storageBucket: "prepwise-2a975.firebasestorage.app",
   messagingSenderId: "412441057073",
-  appId: "1:412441057073:web:99628304701d93723fc210",
-  measurementId: "G-MVQ50W6NEY"
+  appId: process.env.APP_ID,
+  measurementId: process.env.MESAUREMENT_ID
 };
 
 // Initialize Firebase

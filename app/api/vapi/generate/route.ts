a48fe7f3a34d22bd29d21a60a6ac73952c2324e3
saroj580 <chaudharysaroj819@gmail.com>
2025-04-26 00:37:32 +0530

    fix

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,5 +1,7 @@
 import { getRandomInterviewCover } from '@/lib/utils';
 import { google } from '@ai-sdk/google';
+import { db } from "@/firebase/admin";
+import { generateText } from 'ai';
 
 export async function GET() {
     return Response.json({ success: true, data: 'THANK YOU' }, { status: 200 });
@@ -9,7 +11,7 @@ export async function POST(request : Request) {
     const {type, role, level, techstack, amount, userid } = await request.json();
     try {
         const { text : questions } = await generateText({
-             model: google("gemini-2.0-flash-001"),
+            model: google("gemini-2.0-flash-001"),
             prompt: `Prepare questions for a job interview.
             The job role is ${role}.
             The job experience level is ${level}.
@@ -35,7 +37,7 @@ export async function POST(request : Request) {
             createdAt: new Date().toISOString(),
         }
 
-        await db.collection('interviews').add('interview');
+        await db.collection('interviews').add(interview);
         return Response.json({success : true}, {status: 200})
     } catch (error) {
         console.error(error);

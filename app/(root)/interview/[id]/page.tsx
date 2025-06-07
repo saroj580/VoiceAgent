014f05f0033cfef43a0c28bff1479f8f40c06e31
saroj580 <chaudharysaroj819@gmail.com>
2025-06-08 03:29:29 +0530

    page to take the interview that the user is shortlisted in the main page

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Agent from '@/components/Agent';
 import DisplayTechIcons from '@/components/DisplayTechIcons';
+import { getCurrentUser } from '@/lib/actions/auth.action';
 import { getInterviewById } from '@/lib/actions/general.action';
 import { getRandomInterviewCover } from '@/lib/utils';
 import Image from 'next/image';
@@ -6,32 +8,44 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 
 const page = async({ params }: RouteParams) => {
-    const { id } = params;
+    const { id } = await params; // Fix: await params before destructuring
+    const user = await getCurrentUser();
     const interview = await getInterviewById(id);
     
     if (!interview) redirect("/");
     
 
     return (
-        <div className='flex flex-row gap-4 justify-between'>
-            <div className="flex flex-row items-center max-sm:flex-col">
-                <div className='flex flex-row gap-4 items-center'>
-                    <Image
-                        src={getRandomInterviewCover()}
-                        alt="cover-image"
-                        width={40}
-                        height={40}
-                        className='rounded-full object-cover size-[40px]'
-                    />
-                    <h3 className='capitalize'>{interview.role} Interview</h3>
+        <>
+        
+            <div className='flex flex-row gap-4 justify-between'>
+                <div className="flex flex-row items-center max-sm:flex-col">
+                    <div className='flex flex-row gap-4 items-center'>
+                        <Image
+                            src={getRandomInterviewCover()}
+                            alt="cover-image"
+                            width={40}
+                            height={40}
+                            className='rounded-full object-cover size-[40px]'
+                        />
+                        <h3 className='capitalize'>{interview.role} Interview</h3>
+                    </div>
+
+                    <DisplayTechIcons techStack={interview.techstack} />
                 </div>
 
-                <DisplayTechIcons techStack={interview.techstack} />
+                <p className='bg-dark-200 px-4 py-2 rounded-lg capitalize h-fit'>{interview.type}</p>
+
             </div>
-
-            <p className='bg-dark-200 px-4 py-2 rounded-lg capitalize h-fit'>{interview.type}</p>
-
-        </div>
+            <Agent
+                userName={user?.name}
+                type={user?.id}
+                interviewId={id}
+                questions={interview.questions}
+                type='interview'
+                
+            />
+        </>
     )
 }
 
